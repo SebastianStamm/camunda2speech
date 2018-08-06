09a1f5c5f478b19a081000b66e29a3e25a201203
Sebastian Stamm <sebastian.stamm@camunda.com>
2018-08-06 14:38:34 +0200

    add tasklist integration

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # camunda2speech
+
 Control Your Camuda With Your Voice
+
+# Install Frontend
+
+Copy the contents of the frontend folder into a new directory called `speak` in the `app/tasklist/scripts/` folder in your Camunda webapp distribution. For the Tomcat distribution, this would be `server/apache-tomcat-X.X.XX/webapps/camunda/app/tasklist/scripts/speak`.
+
+Add the following content to the `customScripts` object in the `app/tasklist/scripts/config.js` file:
+
+```
+  // …
+  customScripts: {
+    ngDeps: ['tasklist.speak'],
+
+    deps: ['speak'],
+
+    // RequreJS path definitions
+    paths: {
+      'speak': 'scripts/speak/index'
+    }
+  }
+  // …
+```
---
+++ frontend/index.js
@@ -0,0 +1,86 @@
+const endpoint = "http://192.168.80.143:8000/api/guess";
+
+define(["angular"], function(angular) {
+  var ngModule = angular.module("tasklist.speak", []);
+
+  const scriptTag = document.createElement("script");
+  scriptTag.setAttribute(
+    "src",
+    "/camunda/app/tasklist/scripts/speak/lib/hark.bundle.js"
+  );
+  document.head.appendChild(scriptTag);
+
+  const interval = window.setInterval(() => {
+    if (window.hark) {
+      window.clearInterval(interval);
+
+      navigator.mediaDevices
+        .getUserMedia(
+          // constraints - only audio needed for this app
+          {
+            audio: true
+          }
+        )
+
+        // Success callback
+        .then(function(stream) {
+          var options = { threshold: -50 };
+          var speechEvents = hark(stream, options);
+          var mediaRecorder = new MediaRecorder(stream);
+          var chunks = [];
+          mediaRecorder.ondataavailable = function(e) {
+            chunks.push(e.data);
+          };
+
+          speechEvents.on("speaking", function() {
+            console.log("start speaking");
+            mediaRecorder.start();
+          });
+
+          speechEvents.on("stopped_speaking", function() {
+            console.log("stop speaking");
+            mediaRecorder.stop();
+          });
+
+          mediaRecorder.onstop = function(e) {
+            var audio = document.createElement("audio");
+
+            audio.setAttribute("controls", "");
+
+            var blob = new Blob(chunks, { type: "audio/wav" });
+            sendToServer(blob, handleResponse);
+          };
+        })
+
+        // Error callback
+        .catch(function(err) {
+          console.log("The following getUserMedia error occured: " + err);
+        });
+
+      async function sendToServer(blob, response) {
+        const resp = await fetch(endpoint, {
+          method: "POST",
+          body: blob,
+          headers: { "Content-Type": "text/plain" }
+        });
+
+        response(resp);
+      }
+    }
+  }, 100);
+
+  function handleResponse(text) {
+    const processed = text.toLowerCase().trim();
+    if (
+      processed.includes("enable nightmode") ||
+      processed.includes("enable night mode")
+    ) {
+      document.body.style.transition = "filter 1s";
+      document.body.style.filter = "invert(100%)";
+    }
+  }
+
+  window.handleResponse = handleResponse;
+
+  return ngModule;
+});
